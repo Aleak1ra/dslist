29422374a62adbc9e6c32e4fba7eceaa2b0e644f
Aleak1ra <alexandre_bernardi95@hotmail.com>
2023-07-22 18:38:44 -0300

    get games for list

---
+++ src/main/java/com/intensivaojava/dslist/projections/GameMinProjection.java
@@ -0,0 +1,11 @@
+package com.intensivaojava.dslist.projections;
+
+public interface GameMinProjection {
+
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImgUrl();
+    Integer getPosition();
+    String getShortDescription();
+}
